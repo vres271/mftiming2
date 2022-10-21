cdc425222e0434224a7b7bafc165c9911ad36bbd
vres271 <vres271@gmail.com>
2022-10-22 00:11:01 +0400

    crud more

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -33,10 +33,10 @@ export class UsersService {
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.usersRepository.update(id, updateUserDto)
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.usersRepository.delete(id)
   }
 }
