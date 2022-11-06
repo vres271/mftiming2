566daed944804c684985bc00524689e3f904c490
vres271 <vres271@gmail.com>
2022-11-06 21:16:10 +0400

    ???

--- src/racers/entities/racer.entity.ts
+++ src/racers/entities/racer.entity.ts
@@ -1,7 +1,7 @@
 import { RaceEvent } from './../../race-events/entities/race-event.entity';
 import { Race } from './../../races/entities/race.entity';
 import { User } from './../../users/entities/user.entity';
-import { Column, Entity, JoinColumn, JoinTable, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, JoinColumn, JoinTable, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Racer {
@@ -23,5 +23,18 @@ export class Racer {
 
     @OneToMany(() => RaceEvent, (raceEvent) => raceEvent.racer)
     raceEvents: RaceEvent[]
+
+    // @BeforeUpdate()
+    // @BeforeInsert()
+    // convert() {
+    //     this.user= new User;
+    //     this.user.id = this.userId
+    //     this.race= new Race;
+    //     this.race.id = this.raceId
+
+    //     delete this.userId
+    //     delete this.raceId
+    // }
+  
 
 }
--- src/racers/racers.service.ts
+++ src/racers/racers.service.ts
@@ -13,17 +13,8 @@ export class RacersService {
   ) {}
 
   async create(createRacerDto: CreateRacerDto) {
-
-    // const racer = new Racer()
-    const racer = {
-      user: {id:createRacerDto.userId},
-      race: {id:createRacerDto.raceId},
-    }
-
-    return this.racersRepository.save(racer)
+    return this.racersRepository.save(createRacerDto)
       .then(racer=>{
-        // racer.userId = racer.user?.id||0
-        // racer.raceId = racer.race?.id||0
         return racer;
       })
   }
@@ -31,9 +22,6 @@ export class RacersService {
   findAll() {
     return this.racersRepository.find()
       .then(items=>items.map(racer=>{
-        console.log(racer)
-        // racer.userId = racer.user?.id||0
-        // racer.raceId = racer.race?.id||0
         return racer;
       }))
   }
